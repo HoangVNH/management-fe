f14ba7a6562057bf842f537c1d65e7bc2a54b225
Vu, Hoang <hoang.vu@zoi.tech>
2022-08-20 11:18:10 +0700

    resolve webpack error

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,20 @@
-const webpack = require('webpack');
-const { override, addLessLoader, addWebpackPlugin } = require('customize-cra');
+const webpack = require("webpack");
+const { override, addLessLoader, addWebpackPlugin } = require("customize-cra");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = override(
   addWebpackPlugin(
     new webpack.DefinePlugin({
       process: { env: {} },
-    })
+    }),
+    new NodePolyfillPlugin()
   ),
   addLessLoader({
     lessOptions: {
       javascriptEnabled: true,
       modifyVars: {
-        '@font-family': 'Poppins, sans-serif'
-      }
-    }
+        "@font-family": "Poppins, sans-serif",
+      },
+    },
   })
 );
